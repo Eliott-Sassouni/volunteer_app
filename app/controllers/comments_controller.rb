1ef77656e77a29a7e6e80c58b2a346db93d2087c
Eliott-Sassouni <eliott.sassouni@gmail.com>
2015-08-03 17:24:18 +0200

    Added comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+	before_action :authenticate_user!, only: [:create, :destroy]
+	def create
+		@event = Event.find(params[:event_id]) 
+		@comment = @event.comments.build(comment_params)
+		@comment.user = current_user
+		@comment.save
+		redirect_to event_path(@event)
+	end
+	def destroy
+    	@event = Event.find(params[:event_id])
+    	@comment = @event.comments.find(params[:id])
+    	@comment.destroy
+    	redirect_to event_path(@event)
+ end
+
+
+private
+    def comment_params
+      params.require(:comment).permit(:comment)
+    end
+
 end

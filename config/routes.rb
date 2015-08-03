68d0cabebce863ebdbc5ff4bc92ad6631ce2ff2a
menachemdc <menachemdc@gmail.com>
2015-08-03 17:09:20 +0200

    added seed stuff and styling

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+
+  get "welcome/index"
   
   resources :events do
     resources :comments
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+
+user = User.create!  email: "me@me.com", password: "whatever", password_confirmation: "whatever", age: 21, full_name: "Menachem Shemtov", photo: "https://www.facebook.com/photo.php?fbid=10152979864783549&set=a.434970793548.219060.617673548&type=1&theater", organization: true
+User.create!  email: "you@you.com", password: "whatever", password_confirmation: "whatever", age: 30, full_name: "Eliott Sassouni", photo: "https://www.facebook.com/photo.php?fbid=10152979864783549&set=a.434970793548.219060.617673548&type=1&theater", organization: false
+User.create!  email: "us@us.com", password: "whatever", password_confirmation: "whatever", age: 45, full_name: "Andrew Simor", photo: "https://www.facebook.com/photo.php?fbid=10152979864783549&set=a.434970793548.219060.617673548&type=1&theater", organization: true
+
+Event.delete_all
+
+Event.create! name: "Flag Football", description: "Play football with other people.", user_id: user.id, photo: "https://www.facebook.com/photo.php?fbid=10152979864783549&set=a.434970793548.219060.617673548&type=1&theater"

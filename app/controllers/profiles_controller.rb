1ee9edc539a9c17f02a7776234e5ebaec483f0b7
Eliott-Sassouni <eliott.sassouni@gmail.com>
2015-08-05 16:09:25 +0200

    Added description attribute, profiles index and show pages are functional

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,5 @@
 class ProfilesController < ApplicationController
+	before_action :verify_profile, :only => [:edit, :update, :destroy]
 	def index
 		@profiles = Profile.all.order(full_name: :desc)
 	end
@@ -13,8 +14,28 @@ class ProfilesController < ApplicationController
     	@profile = Profile.find(params[:id])
 	end
 
+	def edit
+		@profile = Profile.find(params[:id])
+	end
+
+	def update
+		@profile = Profile.find(params[:id])
+
+		if @profile.update(profile_params)
+			redirect_to profile_path(@profile)
+		else
+			render :edit
+		end
+	end
+
 	private
+	def verify_organization
+		if Profile.find(params[:id]).user != current_user
+			redirect_to profiles_path
+		end
+	end
+
 	def profile_params
-		params.require(:profile).permit(:full_name, :age, :photo)
+		params.require(:profile).permit(:full_name, :age, :photo, :description)
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805125433) do
+ActiveRecord::Schema.define(version: 20150805134417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,8 +45,9 @@ ActiveRecord::Schema.define(version: 20150805125433) do
     t.integer  "age"
     t.string   "full_name"
     t.string   "photo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
@@ -64,9 +65,6 @@ ActiveRecord::Schema.define(version: 20150805125433) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.integer  "age"
-    t.string   "full_name"
-    t.string   "photo"
     t.boolean  "organization"
   end
 

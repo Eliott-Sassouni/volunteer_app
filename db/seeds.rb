795f1df2b7cb1ba2793d9e502692d0690b4df74a
menachemdc <menachemdc@gmail.com>
2015-08-06 00:21:15 +0200

    changed views of events and profiles. Changed seeds to delete comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Comment.delete_all
+
 Profile.delete_all
 
 Event.delete_all
@@ -19,6 +21,6 @@ Profile.create! user_id: user.id, age: 21, full_name: "Menachem Shemtov", photo:
 Profile.create! user_id: user2.id, age: 30, full_name: "Eliott Sassouni", photo: "https://www.facebook.com/photo.php?fbid=10152979864783549&set=a.434970793548.219060.617673548&type=1&theater"
 Profile.create! user_id: user3.id, age: 45, full_name: "Andrew Simor", photo: "https://www.facebook.com/photo.php?fbid=10152979864783549&set=a.434970793548.219060.617673548&type=1&theater"
 
-Event.create! name: "Soup Kitchen", description: "Play football with other people.", user_id: user.id, photo: "http://i.i.cbsi.com/cnwk.1d/i/tim/2013/02/20/football_620x350.jpg"
-Event.create! name: "Flag Football", description: "Play football with other people.", user_id: user.id, photo: "http://i.i.cbsi.com/cnwk.1d/i/tim/2013/02/20/football_620x350.jpg"
-Event.create! name: "5K walk for homeless", description: "Play football with other people.", user_id: user.id, photo: "http://i.i.cbsi.com/cnwk.1d/i/tim/2013/02/20/football_620x350.jpg"
+Event.create! name: "Soup Kitchen", description: "Play football with other people.", user_id: user.id, photo: "http://i.i.cbsi.com/cnwk.1d/i/tim/2013/02/20/football_620x350.jpg", location: "Cape Town"
+Event.create! name: "Flag Football", description: "Play football with other people.", user_id: user.id, photo: "http://i.i.cbsi.com/cnwk.1d/i/tim/2013/02/20/football_620x350.jpg",  location: "Cape Town"
+Event.create! name: "5K walk for homeless", description: "Play football with other people.", user_id: user.id, photo: "http://i.i.cbsi.com/cnwk.1d/i/tim/2013/02/20/football_620x350.jpg",  location: "Cape Town"
